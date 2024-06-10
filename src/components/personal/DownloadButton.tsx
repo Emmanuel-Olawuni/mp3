ccacf3846aa080d509a9f0e721e410021f99f910
Emmanuel <emmanuelolawuni2001@gmail.com>
2024-06-10 18:04:58 +0100

    made changes to the downloadbutton

--- src/components/personal/DownloadButton.tsx
+++ src/components/personal/DownloadButton.tsx
@@ -4,21 +4,13 @@ import { Button, Spinner } from "@nextui-org/react";
 import Link from "next/link";
 
 const DownloadButton = ({ path }: { path: string }) => {
-  const [download, isDownload] = useState<boolean>(false);
-  const handleClick = () => {
-    isDownload(true);
-  };
   return (
     <>
-      {download ? (
-        <Spinner label="Downloading ..." />
-      ) : (
-        <Button className="" onClick={handleClick}>
-          <Link download href={path}>
-            Download Mp3
-          </Link>
-        </Button>
-      )}
+      <Button className="">
+        <Link download href={path}>
+          Download Mp3
+        </Link>
+      </Button>
     </>
   );
 };
