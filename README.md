ae3c2fce2254014c070e9a00ec9d0a297fbd4491
Emmanuel <emmanuelolawuni2001@gmail.com>
2024-06-18 02:02:14 +0100

    update readMe and footer

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
+This is a [Next.js](https://nextjs.org/) project created by with [`Olawuni Emmanuel`](https://techpad.com.ng).
+This a docx to mp3 software
 ## Getting Started
 
 First, run the development server:
--- src/components/personal/Footer.tsx
+++ src/components/personal/Footer.tsx
@@ -1,17 +1,34 @@
-import { LucideInstagram, LucideTwitter } from "lucide-react";
+import {
+  LucideGithub,
+  LucideInstagram,
+  LucideLinkedin,
+  LucideTwitter,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { MdOutlineMail, MdOutlineFacebook } from "react-icons/md";
 const Footer = () => {
-  const date = new Date
-  const year = date.getFullYear()
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <div className=" flex flex-col sm:flex-row justify-around gap-3 py-6 bg-gray-200 px-4 ">
       <div className="social-link flex text-lg text-primary  gap-2">
-        <MdOutlineFacebook  size={24}/>
-        <LucideInstagram size={24} />
-        <LucideTwitter size={24} />
-        <MdOutlineMail size={24} />
+        <LucideGithub
+          href="https://github.com/Emmanuel-Olawuni"
+          target="_blank"
+          size={24}
+        />
+        <LucideLinkedin
+          href="https://www.linkedin.com/in/emmanuelolawuni/"
+          target="_blank"
+          size={24}
+        />
+        <LucideTwitter
+          size={24}
+          href="https://twitter.com/Dev_Emmanuel_"
+          target="_blank"
+        />
+        <MdOutlineMail size={24} href="mailto:admin@techpad.com.ng" />
       </div>
       <div className="copyright"> &copy; {`${year}`}</div>
       <div className="footernote">
