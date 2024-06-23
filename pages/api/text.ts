baf23bc8af77f55785ed6316fa7a4de9af0096ac
Emmanuel <emmanuelolawuni2001@gmail.com>
2024-06-23 12:29:30 +0100

    API changes

--- pages/api/text.ts
+++ pages/api/text.ts
@@ -10,7 +10,7 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const { text, filename, voice } = req.body;
-    const apiKey = process.env.TALKIFY_API_KEY;
+    const apiKey = 'b2847bc7-7470-4b21-ba9d-fbd2edce6476';
 
     if (!text) {
       return res.status(400).json({ error: "Text is required" });
